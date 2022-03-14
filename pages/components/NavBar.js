0ff967376fc24d3ae9cbba63a9dd3c7626da3dc6
siha <es2008e@naver.com>
2022-03-14 20:37:48 +0900

    1.4 v

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,5 +1,7 @@
+import { route } from "next/dist/server/router";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
@@ -7,10 +9,10 @@ export default function NavBar() {
     return (
         <nav>
             <Link href="/">
-                <a style={{color: router.pathname === "/" ? "red" : "blue"}}>Home</a>
+                <a className={` ${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
             </Link>
             <Link href="/about">
-                <a style={{color: router.pathname === "/about" ? "red" : "blue"}}>About</a>
+                <a className={` ${styles.link} ${router.pathname === "/about" ? styles.active : ""}`}>About</a>
             </Link>
         </nav>
     );
