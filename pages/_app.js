1b57c6655a1bd3c0ae7cd942e341e1bd9f2c7f76
siha <es2008e@naver.com>
2022-03-23 20:22:33 +0900

    2.1 v

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
-import NavBar from "./components/NavBar";
+import Layout from "./components/Layout";
+
 
 export default function App ({Component, pageProps}) {
     return (
     <>
-        <NavBar/>
+        <Layout/>
         <Component {...pageProps}/>
-        <style jsx global>{`
-                a {
-                    color: white;
-                }
-            `}</style>
     </>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import NavBar from "./components/NavBar";
+import { useEffect, useState } from "react";
+import Seo from "./components/Seo";
+
+
+const API_KEY = "1f4fd0947ae5dce877fd6e99d18c2c63";
 
 export default function Home() {
-    
+    const [movies, setMovies] = useState();
+    useEffect(()=> {
+        (async () => {
+            const { results } = await (
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                )
+            ).json();
+            setMovies(results);
+        })();
+    },[]);
     return (
         <div>
-            <h1> Hi </h1>
+            <Seo title="Home"/>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map(movie => (
+            <div key={movies.id}>
+                <h4>{movie.original_title}</h4>
+            </div>))}
         </div>
     );
 }
